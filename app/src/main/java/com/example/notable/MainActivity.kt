e0f8bc1d90da805910c71e5b843cfa99bc11da0c
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2025-02-19 22:05:22 +0100

    chore: initial commit

---
+++ app/src/main/java/com/example/notable/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.notable
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.notable.ui.theme.NotableTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NotableTheme {}
+        }
+    }
+}
